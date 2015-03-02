11351df8af5cbfaadd52647c3688cbd8936815d7
yidris <yidris@y-idris-mac.ludia.local>
2015-03-02 17:20:14 -0500

    * Close session at end of request in sqla example

--- example/ext/sqla/resources/__init__.py
+++ example/ext/sqla/resources/__init__.py
@@ -8,6 +8,8 @@ from royal.ext.sqla import Item as SAItem
 
 log = logging.getLogger(__name__)
 
+from pyramid.tweens import EXCVIEW
+
 
 class CollectionBase(SACollection):
     Session = Session
@@ -20,4 +22,18 @@ class ItemBase(SAItem):
 def includeme(config):
 
     config.add_resource('projects')
+    config.add_tween('example.ext.sqla.resources.session_tween_factory',
+                     under=EXCVIEW)
     config.scan(__name__)
+
+
+def remove_session(request):
+    Session.remove()
+
+
+def session_tween_factory(handler, registry):
+    def session_tween(request):
+        request.add_finished_callback(remove_session)
+        response = handler(request)
+        return response
+    return session_tween
