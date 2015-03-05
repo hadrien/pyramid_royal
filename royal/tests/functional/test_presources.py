9564a2b55ae837c6316a106e7dae2acc07e8443f
Hadrien David <hadrien@ectobal.com>
2015-03-05 12:18:46 -0500

    fix test on presources

--- royal/tests/functional/test_presources.py
+++ royal/tests/functional/test_presources.py
@@ -1,20 +1,18 @@
 from royal.tests.functional import TestBase
 
-expected = (
-    ("Resource         Pattern                Collection                      "
-     "           Item"),
-    ("================ ====================== ================================"
-     "========== ================================="),
-    ("photos           /photos                example.resources.photos:"
-     "Collection        example.resources.photos:Item"),
-    ("users            /users                 example.resources.users:"
-     "Collection         example.resources.users:Item"),
-    ("users.followers  /users/(id)/followers  example.resources.users:"
-     "UserFollowers      example.resources.users:Follower"),
-    ("users.photos     /users/(id)/photos     example.resources.users_photos:"
-     "Collection  "),
-    (""),
-)
+expected_lines = {
+    'photos': ('/photos', 'example.resources.photos:Collection',
+               'example.resources.photos:Item'),
+    'projects': ('/projects', 'example.ext.sqla.resources.projects:Collection',
+                 'example.ext.sqla.resources.projects:Item'),
+    'users': ('/users', 'example.resources.users:Collection',
+              'example.resources.users:Item'),
+    'users.followers': ('/users/(id)/followers',
+                        'example.resources.users:UserFollowers',
+                        'example.resources.users:Follower'),
+    'users.photos': ('/users/(id)/photos',
+                     'example.resources.users_photos:Collection'),
+    }
 
 
 class Test(TestBase):
@@ -23,6 +21,21 @@ class Test(TestBase):
         from royal.presources import get_pretty_resource_list
         self.config.commit()
         registry = self.config.registry
-        result = get_pretty_resource_list(registry).split('\n')
-        for index, line in enumerate(result):
-            self.assertEqual(expected[index], line)
+        result = get_pretty_resource_list(registry).splitlines()
+        result.pop(1)  # header underline
+
+        expected = expected_lines.copy()
+        for line in result:
+            terms = line.split()
+
+            if terms[0] not in expected_lines:
+                continue
+
+            columns = expected_lines[terms[0]]
+
+            for term in columns:
+                self.assertIn(term, term)
+
+            expected.pop(terms[0])
+
+        self.assertEqual(expected, {})
